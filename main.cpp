e9afbdeaffde4b1d1201603c0ec2da024d587f94
Sergey Grebenkin <sergey.grebenkin@gmail.com>
2025-01-20 23:52:36 +0100

    Optimized includes in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <string>
-#include <boost/asio.hpp>
-#include <boost/beast.hpp>
-#include <boost/json.hpp>
-#include <filesystem>
-
-#include <boost/asio/ssl.hpp>
-#include <boost/beast/ssl.hpp>
-#include <boost/beast/http.hpp>
-
 #include "config.h"
 #include "TelegramSpeechToTextBot.h"
 
 
 int main() {
-    std::cout << "Bot is running..." << std::endl;
-    std::string lastUpdateId = "0";
-
     TelegramBot tb(BOT_TOKEN);
     tb.run();
 
